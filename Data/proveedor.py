a6e269d51a3391bbdfe7992dd15dad6b3293d05d
MILLERxJ <contactomillerxj@gmail.com>
2023-07-25 17:49:31 -0500

    ok

---
+++ Data/proveedor.py
@@ -0,0 +1,103 @@
+proveedor1=["Nacional de chocolates",620000,-150000]
+proveedor2=["Colanta", 2900000,-400000]
+proveedor3=["Galletas Noel", 1200000,-200000]
+proveedor4=["Pastas  Doria", 5000000,-2000000]
+proveedor5=["Grupo Diana", 7000000,-500000]
+proveedor6=["Rica Palma", 10000000,-3000000]
+proveedor7=["Campo Rico", 11000000,-200000]
+proveedor8=["Macpollo", 5000000,-500000]
+proveedor9=["Nutresa", 15000000,-5000000]
+proveedor10=["Fruveco", 18000000,-6000000]
+proveedor11=["Juan Valdez", 2000000,-400000]
+proveedor12=["CocaCola", 5000000,-400000]
+proveedor13=["Zenu", 8000000,-400000]
+proveedor14=["Haceb", 54000000,-20000000]
+proveedor15=["Colombina", 9000000,-600000]
+proveedor16=["Pepsico", 2000000,-400000]
+proveedor17=["El Indio", 8000000,-400000]
+proveedor18=["Incauca", 8000000,-500000]
+proveedor19=["Providencia", 3000000,-100000]
+proveedor20=["Bucanero", 8000000,-100000]
+proveedor21=[" Ramo", 6000000,-100000]
+proveedor22=["Tramontina", 8000000,-400000]
+proveedor23=["Licorera de Caldas", 30000000,-10000000]
+proveedor24=["Nestle", 8000000,-400000]
+proveedor25=["Huevos Kikes", 28000000,-9000000]
+proveedor26=["Universal", 5000000,-400000]
+proveedor27=["Dersa", 2000000,-400000]
+proveedor28=["Haz de oro", 7000000,-500000]
+proveedor29=["Mundo Huevo", 6000000,-600000]
+proveedor30=["Maizena", 6000000,-400000]
+proveedor31=["La Huerta", 2000000,-400000]
+proveedor32=["Bavaria", 6000000,-400000]
+proveedor33=["Fruco", 9000000,-600000]
+proveedor34=["Fabrica de Licores de Antioquia", 27000000,-4000000]
+proveedor35=["Fab", 25000000,-6000000]
+proveedor36=["Florhuila", 32000000,-6000000]
+proveedor37=["Postobon", 9000000,-800000]
+proveedor38=["Refisal", 7000000,-900000]
+proveedor39=["Royal prestige ", 25000000,-10000000]
+proveedor40=["Axion", 2000000,-400000]
+proveedor41=["Familia", 2000000,-400000]
+proveedor42=["Asovinos", 35000000,-15000000]
+proveedor43=["Bimbo", 7000000,-400000]
+proveedor44=["Pilsen", 18000000,-5000000]
+proveedor45=["CrisLoza", 9000000,-600000]
+proveedor46=["Mimos", 7000000,-600000]
+proveedor47=["Alpina", 9000000,-700000]
+proveedor48=["Sodexo", 6000000,-300000]
+proveedor49=["Fedepan", 8000000,-500000]
+proveedor50=["La Española", 7000000,-600000]
+
+proveedores=[
+    proveedor1,
+    proveedor2,
+    proveedor3,
+    proveedor4,
+    proveedor5,
+    proveedor6,
+    proveedor7,
+    proveedor8,
+    proveedor9,
+    proveedor10,
+    proveedor11,
+    proveedor12,
+    proveedor13,
+    proveedor14,
+    proveedor15,
+    proveedor16,
+    proveedor17,
+    proveedor18,
+    proveedor19,
+    proveedor20,
+    proveedor21,
+    proveedor22,
+    proveedor23,
+    proveedor24,
+    proveedor25,
+    proveedor26,
+    proveedor27,
+    proveedor28,
+    proveedor29,
+    proveedor30,
+    proveedor31,
+    proveedor32,
+    proveedor33,
+    proveedor34,
+    proveedor35,
+    proveedor36,
+    proveedor37,
+    proveedor38,
+    proveedor39,
+    proveedor40,
+    proveedor41,
+    proveedor42,
+    proveedor43,
+    proveedor44,
+    proveedor45,
+    proveedor46,
+    proveedor47,
+    proveedor48,
+    proveedor49,
+    proveedor50,
+]
